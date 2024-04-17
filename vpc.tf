f6166fba5c672abfc6f257265030cf0eb361ea32
Rottingwitch <anaglg9@gmail.com>
2024-04-16 20:19:23 -0500

    test

--- vpc.tf
+++ vpc.tf
@@ -93,7 +93,7 @@ resource "aws_security_group" "sg_public_instance" {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = [var.sg_ingress_cidr]
-    ipv6_cidr_blocks = ["::/0"]
+    # ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
